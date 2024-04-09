92851426f4a82145d2520e0081f7538873262a39
A <51351200+krinknikov@users.noreply.github.com>
2024-04-10 01:49:16 +0300

    index.js 1st update

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+    const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+    const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+    function cardCreate(input, cardDeleteCallback) {
+        const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+        const cardImage = cardElement.querySelector('.card__image');
+        const cardText = cardElement.querySelector('.card__title');  
+        cardImage.src = input.link;
+        cardImage.alt = input.name;
+        cardText.textContent = input.name; 
+        const deleteButton = cardElement.querySelector('.card__delete-button');
+        deleteButton.addEventListener('click', () => {
+            cardDeleteCallback(cardElement);
+        });
+
+        return cardElement;
+    }
 
 // @todo: Функция удаления карточки
+    function cardDelete(cardElement) {
+        cardElement.remove();
+    }
 
 // @todo: Вывести карточки на страницу
+    initialCards.forEach(input => {
+        const cardElement = cardCreate (input, cardDelete);
+        cardlist.append(cardElement);
+    });
